a753732a9ace31f385d814b39bd4eb9f8f6a3c43
komronbek <komronbekobloev@gmail.com>
2024-08-05 17:01:17 +0500

    DJango setup before getting asgi

--- config/asgi.py
+++ config/asgi.py
@@ -1,4 +1,6 @@
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -8,6 +10,7 @@ import apps.notification.routing
 from apps.chat.middleware import JwtAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
